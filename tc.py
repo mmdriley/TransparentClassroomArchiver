300f03622af066104eb6f2911d77ac4314e70d27
Matthew Riley <mdriley@gmail.com>
2022-04-07 00:22:06 -0700

    getting posts, unnecessarily long comment

--- tc.py
+++ tc.py
@@ -20,10 +20,11 @@ class Post(TypedDict):
     html: str
 
 # TODO: sentinel ID or `created_at` for "stop looking"`
-def get_child_posts(s: requests.Session, school_id: int, child_id: int) -> List[Post]:
+def get_child_posts_once(s: requests.Session, school_id: int, child_id: int) -> List[Post]:
     page = 1
     posts: List[Post] = []
     while True:
+        print(f'requesting child {child_id} page {page}')
         r = s.get(f'{API_BASE}/s/{school_id}/children/{child_id}/posts.json?page={page}')
         assert r.status_code == 200, f'get posts failed, status {r.status_code}'
 
@@ -39,6 +40,48 @@ def get_child_posts(s: requests.Session, school_id: int, child_id: int) -> List[
         page = page + 1
 
     return posts
+
+def get_child_posts(s: requests.Session, school_id: int, child_id: int) -> List[Post]:
+    # Retrieve posts twice and check the lists match.
+    #
+    # The only API we have for getting posts is to list by page. While we're
+    # retrieving pages there's a vanishingly small, but nonzero, chance that a
+    # post will be added or deleted.
+    # 
+    # We want to make sure in all circumstances we return *at least* all posts
+    # that existed when we started enumerating and still existed when we're
+    # done. That way, later updates can assume they have all posts older than
+    # the newest post we return.
+    #
+    # An added post wouldn't be too hard to work around. The new post would move
+    # a post that had been on page N to page N+1, so at worst we would see a
+    # post twice on two different pages and we'd need to deduplicate. We might
+    # not get the new post in our listing but that's no worse than if we'd done
+    # the listing right before the post was added.
+    #
+    # Deleted posts are more worrying. If a post on page N is deleted, a post
+    # from page N+1 will shift to page N, and likewise for all later pairs of
+    # adjacent pages. If we're in the midst of retrieving any page after N we
+    # will silently *miss* a post.
+    #
+    # We can imagine some complex schemes to fix this. For example, if we
+    # happened to know the index of the last page of results, we could detect
+    # that a delete happened: retrieve the last page first, then get pages in
+    # order, then finally check if the number of posts on the last page has gone
+    # down. If so, a delete happened, though we don't know where. If the page is
+    # the same size, it's *still possible* that a delete happened if there was
+    # also an add, but we can check if an add occurred by *also* re-retrieving
+    # the first page and seeing if the ID of the first post has changed. (This
+    # assumes new posts will sort first, which seems correct.)
+    #
+    # Instead, we choose an approach that is inefficient but straightforwardly
+    # correct: do the full listing twice and fail if it changes.
+
+    list1 = get_child_posts_once(s, school_id, child_id)
+    list2 = get_child_posts_once(s, school_id, child_id)
+    assert list1 == list2, 'posts changed while listing'
+
+    return list1
 
 def main(username: str, password: str):
     s = requests.Session()
@@ -78,8 +121,8 @@ def main(username: str, password: str):
     children_ids = [x['id'] for x in r.json()]
 
     for child_id in children_ids:
-        pass  # TODO
-        # ps = get_child_posts(s, school_id, child_id)
+        ps = get_child_posts(s, school_id, child_id)
+        print(f'child {child_id}: {len(ps)} posts')
 
     # r = s.get(f'{API_BASE}/s/87/children/99918/posts.json?per_page=50')
     # assert r.status_code == 200, f'failed getting posts, status {r.status_code}\n{r.text}'
